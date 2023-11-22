79dc54c1818f4c7aa265aa6d934ce8538246a399
Sam E <samhith.enganti@frogforce503.org>
2023-11-21 19:53:49 -0500

    added get_base() function to get app base

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,25 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::path;
+use std::env;
 
+fn get_base() -> path::PathBuf {
+    let mut base: path::PathBuf = path::PathBuf::new();
+    if env::consts::OS == "windows" {
+        // windows is C:/.rusty-base
+        base.push("C:\\.rusty-base");
+    } else {
+        // mac and linux is ~/.rusty-base
+        base.push("~/.rusty-base");
+    }
+
+    // make sure base exists. If not, create it
+    if !base.exists() {
+        std::fs::create_dir_all(&base).expect("Failed to create base directory");
+    }
+    base
+}
 
 fn main() {
     tauri::Builder::default()
